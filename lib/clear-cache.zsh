1eed3d002151899f85a3fef0907973474ac7a90d
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-08-29 15:30:13 +0600

    add better printing of pac clean

--- lib/clear-cache.zsh
+++ lib/clear-cache.zsh
@@ -4,18 +4,24 @@ function _pacman_clear_cache() {
     [[ $confirm =~ ^[Yy]$ ]] || return
 
     _pac_print_green "$(_pac_print_bold 'Clearing pacman caches...')"
-    /usr/bin/paccache -vruk1
-    /usr/bin/paccache -vrk2 -c /var/cache/pacman/pkg
+
+    _PACCACHE="$(which paccache)"
+    $_PACCACHE -vruk1
+    $_PACCACHE -vrk2 -c /var/cache/pacman/pkg
 
     local aur_helper="${1:-paru}"
     _pac_print_green "$(_pac_print_bold "Clearing aur ($aur_helper) caches...")"
 
     AUR_CACHE_DIR="$HOME/.cache/$aur_helper/clone"
     AUR_CACHE_REMOVED=$(find "$AUR_CACHE_DIR" -maxdepth 1 -mindepth 1 -type d | xargs -rd'\n' printf "-c%s\n")
-    AUR_REMOVED=$(echo $AUR_CACHE_REMOVED | xargs -rd'\n' /usr/bin/paccache -ruvk0 | sed '/\.cache/!d' | cut -d \' -f2 | xargs -rd'\n' dirname)
-    [ -z "$AUR_REMOVED" ] || _pac_print_gray "$(rm -vrf $AUR_REMOVED)"
+    AUR_REMOVED=$(echo $AUR_CACHE_REMOVED | xargs -rd'\n' $_PACCACHE -ruvk0 | sed '/\.cache/!d' | cut -d \' -f2 | xargs -rd'\n' -n1 dirname)
+    if [[ -z "$AUR_REMOVED" ]]; then
+        while IFS= read -r line; do
+            rm -rf "$line" && echo "removed: '$line'"
+        done <<<"$AUR_REMOVED"
+    fi
 
     AUR_CACHE=$(find "$AUR_CACHE_DIR" -maxdepth 1 -mindepth 1 -type d | xargs -rd'\n' printf "-c%s\n")
-    echo "$AUR_CACHE" | /usr/bin/paccache -vrk2
+    echo "$AUR_CACHE" | $_PACCACHE -vrk2
 
 }
